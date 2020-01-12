650b0e85ab41cac4bd7baa48498e9bd79fab4a00
Filipe Jessé <filipejesse5@gmail.com>
2020-01-12 01:37:02 -0300

    Add IncorrectPageException Class

---
+++ SigaaCrawler/Exceptions/IncorrectPageException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SigaaCrawler.Exceptions
+{
+    class IncorrectPageException : Exception
+    {
+        public IncorrectPageException(string message) 
+            : base(message)
+        {
+
+        }
+    }
+}
