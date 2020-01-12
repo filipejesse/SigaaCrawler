d2b35fe301bf5cad09ae09b76df6ca97867fc384
Filipe Jessé <filipejesse5@gmail.com>
2020-01-12 01:36:10 -0300

    Add ParserBase

---
+++ SigaaCrawler/BaseRobo/ParserBase.cs
@@ -0,0 +1,22 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SigaaCrawler.BaseRobo
+{
+    public class ParserBase
+    {
+        public ParserBase(string html)
+        {
+            this.Html = html;
+
+            this.HtmlDocument = new HtmlDocument();
+            this.HtmlDocument.LoadHtml(html);
+        }
+
+        protected string Html { get; set; }
+
+        protected HtmlDocument HtmlDocument { get; set; }
+    }
+}
