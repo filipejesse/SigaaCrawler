74538f746533301107d038481cab86505c37cf37
Filipe Jessé <filipejesse5@gmail.com>
2020-01-16 23:38:08 -0300

    Update test

--- SigaaCrawlerTest/FonteSigaaTests.cs
+++ SigaaCrawlerTest/FonteSigaaTests.cs
@@ -13,12 +13,14 @@ namespace SigaaCrawlerTest
             Assert.AreEqual(
                 new Result
                 {
+                    Nome = "FILIPE JESSE DE CASTRO ARRUDA",
                     Ira = 7.9695,
                     Curso = "SISTEMAS DE INFORMAÇÃO / JFADEN - Juiz de Fora - BACHARELADO -INT",
                     Matricula = "17006902",
                     Nivel = "Graduação",
                     StatusMatricula = "Ativo",
-                    SemestreEntrada = "2017.1"
+                    SemestreEntrada = "2017.1",
+                    SemestreAtual = "2020.1"
                 }, result);
         }
     }
