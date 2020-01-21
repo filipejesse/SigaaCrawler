14dfebfba42b55294957f0b789f78ccc49b4e738
Filipe Jessé <filipejesse5@gmail.com>
2020-01-20 23:39:41 -0300

    Update program.cs from sigaaCrawler to run the telegramBot

--- SigaaCrawler/Program.cs
+++ SigaaCrawler/Program.cs
@@ -1,13 +1,65 @@
-﻿using System;
+﻿using SigaaCrawler;
+using System;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using Telegram.Bot;
 
 namespace SigaaCrawler
 {
     class Program
     {
+        public static readonly TelegramBotClient Bot = new TelegramBotClient(ConfigurationManager.AppSettings["BotToken"]);
         static void Main(string[] args)
         {
-            var teste = FonteSigaa.StartNavigation();
+            Bot.OnMessage += Bot_OnMessage;
+            Bot.OnMessageEdited += Bot_OnMessage;
 
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+                var match = Regex.Match(e.Message.Text.Trim(), @"(/Dados)\s?:\s?(?<Login>\d+)\s?-\s?(?<Pass>.+)");
+                if (match.Success)
+                {
+                    Result sigaaDados;
+                    try
+                    {
+                        sigaaDados = FonteSigaa.StartNavigation(match.Groups["Login"].Value.Trim(), match.Groups["Pass"].Value.Trim());
+                        var stringData = $"Nome: {sigaaDados.Nome}\n\r" +
+                                    $"Curso: {sigaaDados.Curso}\n\r" +
+                                    $"Ira: {sigaaDados.Ira}\n\r" +
+                                    $"Matricula: {sigaaDados.Matricula}\n\r" +
+                                    $"Nivel: {sigaaDados.Nivel}\n\r" +
+                                    $"Status da Matricula: {sigaaDados.StatusMatricula}\n\r" +
+                                    $"Semestre de Entrada: {sigaaDados.SemestreEntrada}\n\r" +
+                                    $"Semestre Atual: {sigaaDados.SemestreAtual}";
+
+                        Bot.SendTextMessageAsync(e.Message.Chat.Id, stringData);
+                    }
+                    catch (Exception ex)
+                    {
+                        Bot.SendTextMessageAsync(e.Message.Chat.Id, ex.Message);
+                    }
+                }
+
+                else if (e.Message.Text.Trim().Equals("/Bom dia SigBot", StringComparison.OrdinalIgnoreCase))
+                {
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, $"Bom dia {e.Message.Chat.Username}");
+                }
+
+                else
+                {
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, @"Usage: 
+                        /Bom dia SigBot 
+                        /Dados: Login - Senha
+                    ");
+                }
+            }
         }
     }
 }
