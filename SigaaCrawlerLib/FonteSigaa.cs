ed66ee7c9bebbefb10d3271a252c08dc628b0e22
Filipe Jessé <filipejesse5@gmail.com>
2020-01-30 00:31:57 -0300

    Update Fonte to improve request and don't send userAgent statically

--- SigaaCrawlerLib/FonteSigaa.cs
+++ SigaaCrawlerLib/FonteSigaa.cs
@@ -15,58 +15,25 @@ namespace SigaaCrawlerLib
 
         public static Result StartNavigation(string login, string pass)
         {
-            if (string.IsNullOrWhiteSpace(login)) 
-                throw new ArgumentException("Login inválido");
-
-            if (string.IsNullOrWhiteSpace(pass)) 
-                throw new ArgumentException("Senha inválida");
+            ValidateCredentialParameters(login, pass);
 
             var data = GetRequestParameters(login, pass);
             var html = GetResponse(data);
-
-            if (html.Contains("Comportamento Inesperado!"))
-                throw new ComportamentoInesperadoException("Ocorreu um erro inesperado na consulta!");
-
-            if (html.Contains("Usuário e/ou senha inválidos"))
-                throw new InvalidLoginOrPasswordException("Usuário ou senha incorretos.");
-
-            if (!html.Contains("Dados Institucionais"))
-                throw new IncorrectPageException("Erro ao navegar para site. Página encontrada não é válida");
-
+            ValidateResponse(html);
 
             var parser = new ParserSigaa(html);
-
             var result = parser.GetData();
 
             return result;
         }
 
-        private static string GetResponse(string data)
+        private static void ValidateCredentialParameters(string login, string pass)
         {
-            var contentType = "application/x-www-form-urlencoded";
-            var userAgent = "PostmanRuntime/7.21.0";
-            var cookie = new Cookie()
-            {
-                Name = "JSESSIONID",
-                Value = "1216A54C01C149AE600E3CD3642B9036.node8",
-                Domain = "sig.ifsudestemg.edu.br",
-                Secure = true,
-                HttpOnly = true,
-                Path = "/"
-            };
+            if (string.IsNullOrWhiteSpace(login))
+                throw new ArgumentException("Login inválido");
 
-            string html = string.Empty;
-            var retries = 0;
-            var maxRetries = 3;
-            while (retries <= maxRetries)
-            {
-                html = HttpClient.Post(BaseUrl, data, contentType, cookie, userAgent, out var status);
-                if (status == HttpStatusCode.OK)
-                    break;
-
-                retries++;
-            }
-            return html;
+            if (string.IsNullOrWhiteSpace(pass))
+                throw new ArgumentException("Senha inválida");
         }
 
         private static string GetRequestParameters(string user, string password)
@@ -75,6 +42,36 @@ namespace SigaaCrawlerLib
             var senha = Uri.EscapeDataString(password);
             var dispatch = Uri.EscapeDataString("logOn");
             return $"dispatch={dispatch}&user.login={login}&user.senha={senha}&width=1024&height=768";
+        }
+
+        private static string GetResponse(string data)
+        {
+            var contentType = "application/x-www-form-urlencoded";
+            var cookie = new Cookie("JSESSIONID", "1216A54C01C149AE600E3CD3642B9036.node8", "/", "sig.ifsudestemg.edu.br");
+            string html = string.Empty;
+            var retries = 0;
+            var maxRetries = 3;
+            while (retries <= maxRetries)
+            {
+                html = HttpClient.Post(BaseUrl, data, contentType, cookie, out var status);
+                if (status == HttpStatusCode.OK)
+                    break;
+
+                retries++;
+            }
+            return html;
+        }
+
+        private static void ValidateResponse(string html)
+        {
+            if (html.Contains("Comportamento Inesperado!"))
+                throw new ComportamentoInesperadoException("Ocorreu um erro inesperado na consulta!");
+
+            if (html.Contains("Usuário e/ou senha inválidos"))
+                throw new InvalidLoginOrPasswordException("Usuário ou senha incorretos.");
+
+            if (!html.Contains("Dados Institucionais"))
+                throw new IncorrectPageException("Erro ao navegar para site. Página encontrada não é válida");
         }
     }
 }
